3160aa931f9a28789306b0f593529b1febe37d36
jdunca <jamie.duncan@mail.utoronto.ca>
2023-02-23 17:40:11 -0500

    updated data prep file to resolve issues

--- data-prep.R
+++ data-prep.R
@@ -7,37 +7,60 @@ library(tibble)
 
 ### read in archive log prepped in `archive prototype`
 archived <- read_csv('misc/ia-uploaded.csv') %>% 
-  mutate(agency = str_extract(identifier, ".+(?=_)")) %>% 
+  mutate(agency = str_extract(identifier, ".+(?=_)"),
+         title = paste0(str_extract(title, "A-\\d{4}"), "-", sprintf("%05d", as.numeric(str_extract(title, "(?<!A-\\d{4})\\d+$")))
+         )) %>% 
   select(identifier, title, agency, pinpoint, internet_archive) %>% 
   rename(request_number = title,
          archive = internet_archive) %>% 
   select(-pinpoint)
 
 ### read in full summary data from Lucas' github
-ati_summaries.read <- read_csv("https://raw.githubusercontent.com/lchski/gc-ati-summaries-data/main/ati-summaries.csv") %>% select(-umd_number) %>%
+
+summaries.raw <- read_csv("https://raw.githubusercontent.com/lchski/gc-ati-summaries-data/main/ati-summaries.csv")
+
+
+ati_summaries.read <-  summaries.raw %>% 
+  select(-umd_number) %>%
   mutate(agency = if_else(str_detect(owner_org, "-"), #create agency variable for join
                           str_to_upper(str_extract(owner_org, ".+(?=-)")),
                           str_to_upper(owner_org)),
-         agency = if_else(agency == "JUS", "DOJ", agency),#rename JUS to DOJ
-         request_number = case_when(str_detect(request_number, "/ A-") ~ 
-                                      str_extract(request_number, "A-\\d{4}-\\d{5}"),
-                                    TRUE ~ request_number))
+         agency = if_else(agency == "JUS", "DOJ", agency),#,#rename JUS to DOJ
+         request_number = if_else(str_detect(request_number, "A-\\d{4}-\\d{1,5}"), 
+                                  str_extract(request_number, "A-\\d{4}-\\d{1,5}"), request_number), #extract normal request numbers
+         request_number = str_remove(request_number, "\\/.+$"), #remove everything after "/"
+         request_number = if_else(str_detect(request_number, "A-\\d{4}"),
+                                  paste0(str_extract(request_number, "A-\\d{4}"), "-", 
+                                         sprintf("%05d",as.numeric(str_extract(request_number, "(?<=A-\\d{4}-)\\d+")))), # add leading zeros
+                                  request_number),
+         request_number = if_else(str_detect(request_number, "A\\d"))
+  )
+         
+        
+class(ati_summaries.read$agency)
 
-#read in pinpoint files
-pinpoint <- tibble(read_delim('misc/on_pinpoint.txt', delim = "\n", col_names = FALSE)) %>% 
-  mutate(identifier = str_remove(X1, "\\..+$"),
-         pinpoint = TRUE) %>% 
-  select(-X1)
+
   
 
 ### join archive log with full summary data
 ati_summaries.join <- ati_summaries.read %>%
-  full_join(archived, by = c('request_number', 'agency')) %>%
-  mutate(identifier = case_when(is.na(identifier) ~ paste(agency, request_number, sep = "_"),
-                                TRUE ~ identifier)) %>% 
-  full_join(pinpoint, by = 'identifier') %>% 
-  mutate(archive = if_else(is.na(archive), FALSE, archive),
-         pinpoint = if_else(is.na(pinpoint), FALSE, pinpoint))
+  full_join(archived, by = c('request_number', 'agency'))
+
+
+
+
+
+
+  #mutate(identifier = case_when(is.na(identifier) ~ paste(agency, request_number, sep = "_"),
+                                #TRUE ~ identifier)) %>% 
+  #full_join(pinpoint, by = 'identifier') %>% 
+  mutate(archive = if_else(is.na(archive), FALSE, archive))#,
+         #pinpoint = if_else(is.na(pinpoint), FALSE, pinpoint))
+
+test <- ati_summaries.read %>% 
+  filter(agency == 'INFC')
+
+anti_join(archived, test, by = 'identifier')
 
 # merge year and month and convert to 'date' format
 ati_summaries <- ati_summaries.join %>%
@@ -101,3 +124,11 @@ ati_summaries_en <- ati_summaries %>%
 ati_summaries_fr <- ati_summaries %>%
   select(date, request_number, summary_fr, disposition, pages, org_ac_fr, org_fr, pinpoint, archive
   )
+
+
+
+#read in pinpoint files
+#pinpoint <- tibble(read_delim('misc/on_pinpoint.txt', delim = "\n", col_names = FALSE)) %>% 
+# mutate(identifier = str_remove(X1, "\\..+$"),
+#         pinpoint = TRUE) %>% 
+# select(-X1)
